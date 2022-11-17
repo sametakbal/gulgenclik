b7d4a0ebe42412c712e8d751da1cdbe46f5d22cd
Samet AKBAL <sametakbal7@gmail.com>
2022-11-17 21:16:51 +0300

    tech stack changed

--- gulgenclik/Models/Table.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace gulgenclik.Models
-{
-	public class Table : BaseEntity
-	{
-		public string? Name { get; set; }
-	}
-}
-
---
+++ src/main/java/com/akbal/gulgenclik/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.akbal.gulgenclik.repository;
+
+import com.akbal.gulgenclik.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
